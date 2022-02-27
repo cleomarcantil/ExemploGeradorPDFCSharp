1a999ca995c40a62ded9f660499d406838e3db7c
Cleomar Cantil <cleomarcantil@outlook.com>
2022-02-27 20:21:14 -0300

    Adição da dependencia libSkiaSharp para Linux.

--- ExemploGeradorPDFCSharp/Program.cs
+++ ExemploGeradorPDFCSharp/Program.cs
@@ -2,6 +2,7 @@
 using ExemploGeradorPDFCSharp.GeraPDF;
 using QuestPDF.Fluent;
 using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 Console.WriteLine("Exemplo de Geração de PDFs com CSharp usando o pacote QuestPDF.");
 
@@ -9,6 +10,7 @@ Console.WriteLine($"(1) - Gerar relatório de pessoas");
 
 Console.Write($"Escolha uma opção: ");
 var opcao = Console.ReadKey();
+Console.WriteLine();
 
 var filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "teste.pdf");
 
@@ -25,4 +27,12 @@ switch (opcao.KeyChar)
 		return;
 }
 
-Process.Start("explorer.exe", filePath);
+Console.WriteLine($"Arquivo gerado: '{Path.GetFileName(filePath)}'.");
+
+
+if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+	Process.Start("explorer.exe", filePath);
+else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+	Process.Start("xdg-open", filePath);
+else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+	Process.Start("open", filePath);
